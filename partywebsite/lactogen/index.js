65e302b1334c2a0944ac34e51930d01e820e11d7
AbhishekSuraj0 <ab8948464510gmmail.com>
2026-05-26 11:18:34 +0530

    sdf

--- partywebsite/lactogen/index.js
+++ partywebsite/lactogen/index.js
@@ -1,21 +1,148 @@
 
 
-const url = "https://opensheet.elk.sh/1G5kY3GGIv-wyA8qq-Um_SazeQgzUzyVMCfRtXXAzrVA/Dailymilk"
+const url = "https://opensheet.elk.sh/1G5kY3GGIv-wyA8qq-Um_SazeQgzUzyVMCfRtXXAzrVA/Dailymilk";
+
+var tables = document.getElementsByClassName("tables")[0]
 
 fetch(url)
     .then(res => res.json())
     .then(data => {
         data.map(u => {
             const div = document.createElement('div')
-            div.innerHTML = `
-            <img width="200px" src="./images/${u.image}.png" alt=""><br>
-            <span>${u.ProductName}</span><br>
-            <span>MRP : - ${u.Productprice}</span><br>
-            <span>Rate :- ${u.rate}</span>
-            
-            `
+            div.classList = "showdiv"
+            div.innerHTML = `       
+                    <div id="showitmes">
+                        <img  src="./images/${u.image}.png" alt="">
+                        <span>${u.ProductName}</span>
+                        <div style="display: flex;justify-content: space-evenly; gap: 20px;">
+                            <span><strike>MRP ${u.Productprice}</strike></span>
+                            <span>Rate : <Span>${u.rate}</Span></span>
+                        </div>
+                        <div  id="hidde">Add</div>
+                        <div class="minusplus">
+                            <button id="minus">-</button>
+                            <label id="valueitmes" for="">0</label>
+                            <button id="plus">+</button>
+                        </div>
+                    </div>     ` ;
 
-            document.getElementById("listitmes").append(div)
+            document.getElementById("listitmes").append(div);
+
+            let addbtn = div.querySelector("#hidde");
+            let minusplus = div.querySelector(".minusplus");
+
+
+            var num = 1;
+
+            let minus = div.querySelector("#minus");
+            let valueitmes = div.querySelector("#valueitmes");
+            let plus = div.querySelector("#plus");
+
+            addbtn.addEventListener("click", () => {
+                addbtn.style.display = "none";
+                minusplus.style.display = "block"
+            });
+
+            plus.addEventListener("click", () => {
+                if (num <= 0) {
+                    num = 0
+                }
+                let found = false;
+                for (let i = 1; i < tables.rows.length; i++) {
+                    let cellValue = tables.rows[i].cells[0].innerText;
+                    if (cellValue == u.ProductName) {
+                        tables.rows[i].cells[1].innerText = num;
+                        found = true;
+                        break;
+                    }
+                }
+                // if row not found then create new row
+                if (!found) {
+                    let row = tables.insertRow();
+                    let cell1 = row.insertCell(0);
+                    let cell2 = row.insertCell(1);
+                    cell1.innerText = u.ProductName;
+                    cell2.innerText = num;
+                }
+                valueitmes.innerText = num++;
+            })
+
+            minus.addEventListener("click", () => {
+                valueitmes.innerText = num--
+                if (num <= 0) {
+                    num = 0
+                }
+
+                let found = false;
+                for (let i = 1; i < tables.rows.length; i++) {
+                    let cellValue = tables.rows[i].cells[0].innerText;
+                    if (cellValue == u.ProductName) {
+                        tables.rows[i].cells[1].innerText = num;
+                        found = true;
+                        break;
+                    }
+                }
+                // if row not found then create new row
+                if (!found) {
+                    let row = tables.insertRow();
+                    let cell1 = row.insertCell(0);
+                    let cell2 = row.insertCell(1);
+                    cell1.innerText = u.ProductName;
+                    cell2.innerText = num;
+                }
+
+            })
         })
     })
     .catch(err => console.log(err))
+
+document.getElementById("ordernow").addEventListener("click", async () => {
+    let bill = document.getElementById("billdata");
+    var shopname = document.getElementById("shopname").value
+
+    if (!shopname) {
+        alert("as")
+    } else {
+        html2canvas(bill).then(async (canvas) => {
+            canvas.toBlob(async (blob) => {
+                let file = new File(
+                    [blob],
+                    "bill.png",
+                    { type: "image/png" }
+                );
+                // mobile share
+                if (navigator.share) {
+                    await navigator.share({
+                        title: "Bill",
+                        text: "Deepu Medical Bill",
+                        files: [file]
+                    });
+                } else {
+                    alert("WhatsApp share not supported");
+                }
+            });
+
+        });
+    }
+
+
+
+    // div to canvas
+
+
+});
+
+document.getElementById("shopname").addEventListener("input", () => {
+
+    var shopname = document.getElementById("shopname").value
+    document.querySelector("#billdata label").textContent = `Shop Name :- ${shopname}`
+
+
+
+})
+
+
+
+
+
+
