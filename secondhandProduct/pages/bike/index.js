012ca99a6fe514c601f09bf726b30293f78c7606
AbhishekSuraj0 <ab8948464510gmmail.com>
2026-06-16 10:50:53 +0530

    ss

--- secondhandProduct/pages/bike/index.js
+++ secondhandProduct/pages/bike/index.js
@@ -1,4 +1,4 @@
-function loadFile(fileName, elementId){
+function loadFile(fileName, elementId) {
     fetch(fileName)
         .then(res => res.text())
         .then(data => {
@@ -6,35 +6,55 @@ function loadFile(fileName, elementId){
         });
 }
 
+var products = "bike"
+
 loadFile("../../components/header.html", "header");
 loadFile("../../components/footer.html", "footer");
 
+var productdeatiler = []
 
 const activesheet = "1yqyPnBtwf6gUhOT3wFDk-FaQzSo6LfsnnDNm5WFZUE8";
 const maindiv = document.getElementsByClassName("maindiv")[0];
 
 fetch(`https://opensheet.elk.sh/${activesheet}/sellerproduct`)
-.then(res => res.json())
-.then(data => {
-    // Using forEach instead of map since we are not returning a new array
-    data.forEach((d) => {
-        var div = document.createElement('div');
-        div.classList.add("maindivchild");
-        
-        // Dynamic string matching your Google Sheet keys
-        // Replaced hardcoded values with variables like d.productname, d.mrp, d.rate
-        div.innerHTML = `
+    .then(res => res.json())
+    .then(data => {
+
+        var filterdata = data.filter(a => a.catageories === `${products}`);
+
+        // Using forEach instead of map since we are not returning a new array
+        filterdata.forEach((d) => {
+            var div = document.createElement('div');
+            div.classList.add("maindivchild");
+            // Dynamic string matching your Google Sheet keys
+            // Replaced hardcoded values with variables like d.productname, d.mrp, d.rate
+            div.innerHTML = `
             <img width="100px" src="../../images/${d.viewimage}.jpg" alt="${d.productname || 'product'}">
             <label>${d.name || 'Product Name'}</label>
             <div class="gr">
                 <span>MRP: ${d.mrp || '0'}</span>
-                <span>Rate: ${d.rate || '0'}</span>
+                <span>Rate: ${d.price || '0'}</span>
             </div>
             <div class="sharebtn">
                 <button>Viiew More</button>
             </div>
         `;
-        
-        maindiv.append(div);
+            maindiv.append(div);
+
+            div.addEventListener("click",()=>{
+
+                document.getElementsByClassName("productviews")[0].style.display = "block";
+                document.getElementById("productviewname").innerHTML = `${d.name}`
+                document.getElementById("productviewdiscripiition").innerHTML = `${d.description}`
+                document.getElementById("viewmrp").innerHTML = `MRP : ${d.mrp}`
+                document.getElementById("viewrate").innerHTML = `Rate : ${d.price}`
+            })
+
+    
+        });
     });
-});
+
+document.getElementById("closebtn").addEventListener("click", () => {
+    document.getElementsByClassName("productviews")[0].style.display = "none"
+})
+
