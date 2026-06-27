0437763684b0d5aacfea327c6033eadb8e5eb20b
AbhishekSuraj0 <ab8948464510gmmail.com>
2026-06-27 12:34:59 +0530

    fff

--- statusCustomer/index.js
+++ statusCustomer/index.js
@@ -36,16 +36,16 @@ function showdiv(k) {
     // Check if the filtered array contains any matching records
     if (k.length > 0) {       
 
-        if (k[0].status === "pending") {
+        if (k[0].status == "pending") {
             showDiv.style.display = "block";
             document.getElementById("h1text").innerHTML = `Status Pending hai`
             document.getElementById("h1text").style.color = "#054f77"
-            document.getElementById("ptext").innerHTML = `Abhi aap ka medicine aa raha hai`
+            document.getElementById("ptext").innerHTML = `Hello ${k[0].name} Ji , Abhi aap ka medicine aa raha hai`
         } else {
             showDiv.style.display = "block";
             document.getElementById("h1text").innerHTML = `congratulations`
             document.getElementById("h1text").style.color = "Green"
-            document.getElementById("ptext").innerHTML = `App ki Medicine Shop pe aa chuka hai , Please Pickup`
+            document.getElementById("ptext").innerHTML = `Hello ${k[0].name} Ji , app ki Medicine Shop pe aa chuka hai , Please Pickup`
         }
        
 
