cfae70f5159097eee4d1b18e2dc5b53f98b43228
AbhishekSuraj0 <ab8948464510gmmail.com>
2026-06-30 09:41:18 +0530

    sssasads

---
+++ agentWork/index.js
@@ -0,0 +1,43 @@
+var numberInput = document.getElementById("numberInput")
+var sheet = "1G5kY3GGIv-wyA8qq-Um_SazeQgzUzyVMCfRtXXAzrVA"
+
+
+let po = null;
+
+document.getElementById("agentData").style.display = "none"
+
+
+function searchbtn() {
+    if (numberInput.value && numberInput.value.length >= 10) {
+        fetch(`https://opensheet.elk.sh/${sheet}/agentData`)
+            .then(res => res.json())
+            .then(d => {
+                var ud = d.filter(dv => dv.number == numberInput.value)
+                po = ud;
+                profiles(ud)
+
+
+            })
+            .catch(error => console.error("Error fetching sheet data:", error));
+    } else {
+        alert("nahi hai")
+    }
+}
+
+function profiles(d) {
+    document.getElementById("agentData").style.display = ""
+    document.getElementById("profilesImage").src = `./images/${d[0].image}`;
+    document.getElementById("profileName").innerText = `${d[0].name}`;
+    document.getElementById("profileAddress").innerText = `Address : ${d[0].address} `;
+    document.getElementById("profileWallat").innerText = `Your Money : ${d[0].money}`;
+    numberInput.value = ""
+}
+
+document.getElementById("withdrawBtn").addEventListener("click", () => {
+    let m = `Mera naam ${po[0].name} hai. Kripya mera ₹${po[0].money} bank account mein withdraw kar dijiye.`;
+
+    window.open(
+        `https://wa.me/916387215755?text=${encodeURIComponent(m)}`,
+        "_blank"
+    );
+});
